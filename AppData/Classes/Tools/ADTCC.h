8279a6e318d58f38da6a7411f7cacd558fe53ff1
888pozin-coder <888pozin@gmail.com>
2026-03-24 18:08:27 +0800

    Update ADTCC.h

--- AppData/Classes/Tools/ADTCC.h
+++ AppData/Classes/Tools/ADTCC.h
@@ -2,17 +2,13 @@
 //  TCC.h
 //  AppData
 //
-//  Created by udevs on 21/11/2020.
-//
 
 #ifndef ADTCC_h
 #define ADTCC_h
 
 #import <Foundation/Foundation.h>
 
-FOUNDATION_EXTERN NSString *const kTCCServiceAll;
-
-// 修改为直接传入 Bundle Identifier，摒弃旧的 CFBundleRef
+// 彻底去掉 kTCCServiceAll 的声明，避免重复符号冲突
 FOUNDATION_EXTERN int TCCAccessResetForBundleIdentifier(NSString *service, NSString *bundleIdentifier);
 FOUNDATION_EXTERN NSArray<NSDictionary *> *TCCAccessCopyInformationForBundleIdentifier(NSString *bundleIdentifier);
 
