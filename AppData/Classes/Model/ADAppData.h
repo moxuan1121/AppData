e14fcc24fec280438a16bce757198598d7c7cfc2
888pozin-coder <888pozin@gmail.com>
2026-03-25 02:20:58 +0800

    Update ADAppData.h

--- AppData/Classes/Model/ADAppData.h
+++ AppData/Classes/Model/ADAppData.h
@@ -16,15 +16,9 @@
 @interface ADAppData : NSObject
 
 @property (nonatomic, strong) SBIconView *iconView;
-
 @property (nonatomic, strong) UIImage *iconImage;
 
 - (NSString *)name;
-// === 新增：为兼容 TSDowngradeManager 桥接的属性 ===
-- (NSString *)displayName;
-- (NSString *)bundlePath;
-// ===========================================
-
 @property (nonatomic, strong) NSString *version;
 @property (nonatomic, strong) NSString *bundleIdentifier;
 
@@ -51,33 +45,40 @@
 - (BOOL)isApplication;
 
 #pragma mark - Icon Name
-
 - (NSString *)customIconName;
 - (void)setCustomIconName:(NSString *)name;
 
 #pragma mark - AppStore
-
 - (BOOL)hasAppStoreApp;
 - (void)openInAppStore;
-#pragma mark - Permissions
 
+#pragma mark - Permissions
 - (NSArray <NSDictionary *> *)getPermissions;
 - (void)resetAllAppPermissions;
 
 #pragma mark - Reset App
 - (void)getAppUsageDirectorySizeWithCompletion:(void(^)(NSString *formattedSize))completion;
 - (void)resetDiskContentWithCompletion:(void(^)())completion;
-#pragma mark - Caches
 
+#pragma mark - Caches
 - (void)getCachesDirectorySizeWithCompletion:(void(^)(NSString *formattedSize))completion;
 - (void)clearAppCachesWithCompletion:(void(^)())completion;
 
 #pragma mark - App Badges
-
 - (void)setAppBadgeCount:(NSInteger)badgeCount;
 - (NSInteger)appBadgeCount;
-#pragma mark - Uninstall App
 
+#pragma mark - Uninstall App
 - (void)uninstallAppWithCompletion:(void(^)(BOOL success))completion;
+
+#pragma mark - Downgrade & Verification (原生降级组件)
+// 校验当前账号与下载账号是否匹配
+- (BOOL)isAppOwnershipVerified;
+// 获取应用 TrackID
+- (void)fetchDowngradeTrackIDWithCompletion:(void(^)(long long trackId, NSError *error))completion;
+// 获取应用历史版本
+- (void)fetchDowngradeVersionsForTrackID:(long long)trackId completion:(void(^)(NSArray *versions, NSError *error))completion;
+// 触发 App Store 降级下载
+- (void)performDowngradeWithTrackID:(long long)trackId versionID:(long long)versionId controller:(UIViewController *)controller;
 
 @end
