69fbc9c2eee6ca76deef9f7df3679bf49bea0a28
888pozin-coder <888pozin@gmail.com>
2026-03-24 21:31:45 +0800

    Add files via upload

--- AppData/Classes/Model/ADAppData.m
+++ AppData/Classes/Model/ADAppData.m
@@ -11,11 +11,11 @@
 #import "ADTerminator.h"
 #import <dlfcn.h>
 #import <Foundation/Foundation.h>
-#import <sqlite3.h>
-#import <spawn.h>
+
 
 @interface ADAppData ()
 @property (nonatomic, strong) SBApplication *sbApplication;
+
 @property (nonatomic, strong) LSApplicationProxy *appProxy;
 @end
 
@@ -105,8 +105,7 @@
     // Disk Usage
     if ([self.appProxy respondsToSelector:@selector(staticDiskUsage)]) {
         self.diskUsage = [self.appProxy.staticDiskUsage integerValue];
-        self.diskUsageString = [NSByteCountFormatter stringFromByteCount:[self.appProxy.staticDiskUsage longLongValue]
-                                                              countStyle:NSByteCountFormatterCountStyleFile];
+        self.diskUsageString = [NSByteCountFormatter stringFromByteCount:[self.appProxy.staticDiskUsage longLongValue] countStyle:NSByteCountFormatterCountStyleFile];
     }
     
     // Info for more page
@@ -117,7 +116,6 @@
     // Other Info
     self.entitlements = self.appProxy.entitlements;
     self.entitlementsIdentifiers = self.entitlements.allKeys;
-    
     ASYNC({
         NSURL *infoPlistURL = [self.bundleURL URLByAppendingPathComponent:@"Info.plist"];
         NSDictionary *infoDictionary = [NSDictionary dictionaryWithContentsOfURL:infoPlistURL];
@@ -188,10 +186,8 @@
 }
 
 - (void)openInAppStore {
-    NSString *appStoreLink = [NSString stringWithFormat:@"itms-apps://apps.apple.com/app/id%@", self.appProxy.itemID];
-    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:appStoreLink]
-                                       options:@{}
-                             completionHandler:nil];
+    NSString *appStoreLink = [NSString stringWithFormat:@"itms-apps://apps.apple.com/app/id%@",self.appProxy.itemID];
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:appStoreLink] options:@{} completionHandler:nil];
 }
 
 #pragma mark - Caches
@@ -219,7 +215,7 @@
 - (void)getCachesDirectorySizeWithCompletion:(void(^)(NSString *formattedSize))completion {
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
         unsigned long long int totalSize = 0;
-        NSArray<NSURL *> *cacheDirectoriesURLs = [self cacheDirectoriesURLs];
+        NSArray <NSURL *> *cacheDirectoriesURLs = [self cacheDirectoriesURLs];
         for (NSURL *url in cacheDirectoriesURLs) {
             if (url && [[NSFileManager defaultManager] fileExistsAtPath:url.path]) {
                 unsigned long long int folderSize = 0;
@@ -227,17 +223,17 @@
                 totalSize += folderSize;
             }
         }
-        NSString *formattedSize = [NSByteCountFormatter stringFromByteCount:totalSize
-                                                                 countStyle:NSByteCountFormatterCountStyleFile];
+        NSString *formattedSize = [NSByteCountFormatter stringFromByteCount:totalSize countStyle:NSByteCountFormatterCountStyleFile];
         dispatch_async(dispatch_get_main_queue(), ^{
             completion(formattedSize);
         });
     });
 }
 
-- (void)clearAppCachesWithCompletion:(void(^)(void))completion {
+- (void)clearAppCachesWithCompletion:(void(^)())completion {
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-        NSArray<NSURL *> *cacheDirectoriesURLs = [self cacheDirectoriesURLs];
+
+        NSArray <NSURL *> *cacheDirectoriesURLs = [self cacheDirectoriesURLs];
         for (NSURL *url in cacheDirectoriesURLs) {
             [self.class deleteContentsOfDirectoryAtURL:url];
         }
@@ -250,10 +246,7 @@
 
 + (void)deleteContentsOfDirectoryAtURL:(NSURL *)url {
     NSFileManager *fm = [NSFileManager defaultManager];
-    NSDirectoryEnumerator *enumerator = [fm enumeratorAtURL:url
-                                 includingPropertiesForKeys:nil
-                                                    options:NSDirectoryEnumerationSkipsSubdirectoryDescendants
-                                               errorHandler:nil];
+    NSDirectoryEnumerator *enumerator = [fm enumeratorAtURL:url includingPropertiesForKeys:nil options:NSDirectoryEnumerationSkipsSubdirectoryDescendants errorHandler:nil];
     NSURL *child;
     while ((child = [enumerator nextObject])) {
         [fm removeItemAtURL:child error:NULL];
@@ -273,98 +266,43 @@
 
 - (void)setAppBadgeCount:(NSInteger)badgeCount {
     if ([self.sbApplication respondsToSelector:@selector(setBadgeValue:)]) {
-        [self.sbApplication setBadgeValue:@(badgeCount)];
+        [self.sbApplication setBadgeValue:[NSNumber numberWithInteger:badgeCount]];
     } else {
-        [self.sbApplication setBadgeNumberOrString:@(badgeCount)];
+        [self.sbApplication setBadgeNumberOrString:[NSNumber numberWithInteger:badgeCount]];
     }
 }
 
 #pragma mark - Permissions
-
-// 辅助方法：重启 tccd 守护进程，让直接修改的数据库立即生效
-- (void)restartTCCD {
-    pid_t pid;
-    const char *args[] = {"killall", "-9", "tccd", NULL};
-    posix_spawn(&pid, "/usr/bin/killall", NULL, NULL, (char *const *)args, NULL);
-}
-
-- (NSArray<NSDictionary *> *)getPermissions {
-    if (@available(iOS 16.0, *)) {
-        // iOS 16 及以上：直接查询 TCC.db 数据库
-        NSMutableArray *permissions = [NSMutableArray array];
-        NSString *dbPath = @"/private/var/mobile/Library/TCC/TCC.db";
-        sqlite3 *db;
-        
-        if (sqlite3_open([dbPath UTF8String], &db) == SQLITE_OK) {
-            NSString *query = [NSString stringWithFormat:@"SELECT service FROM access WHERE client = '%@'", self.bundleIdentifier];
-            sqlite3_stmt *statement;
-            if (sqlite3_prepare_v2(db, [query UTF8String], -1, &statement, NULL) == SQLITE_OK) {
-                while (sqlite3_step(statement) == SQLITE_ROW) {
-                    const unsigned char *service = sqlite3_column_text(statement, 0);
-                    if (service) {
-                        NSString *serviceStr = [NSString stringWithUTF8String:(const char *)service];
-                        [permissions addObject:@{@"service": serviceStr}];
-                    }
-                }
-                sqlite3_finalize(statement);
-            }
-            sqlite3_close(db);
-        }
-        return permissions;
-    } else {
-        // iOS 15 及以下：使用原有的私有 API
-        CFBundleRef bundle = CFBundleCreate(kCFAllocatorDefault, (CFURLRef)self.appProxy.bundleURL);
-        if (bundle) {
-            NSArray *information = TCCAccessCopyInformationForBundle(bundle);
-            CFRelease(bundle);
-            return information;
-        }
-        return nil;
+// 还原：这里回退为了原版基于 CFBundleRef 的代码
+- (NSArray <NSDictionary *> *)getPermissions {
+    CFBundleRef bundle = CFBundleCreate(kCFAllocatorDefault, (CFURLRef)self.appProxy.bundleURL);
+    if (bundle) {
+        NSArray *information = TCCAccessCopyInformationForBundle(bundle);
+        CFRelease(bundle);
+        return information;
     }
+    return nil;
 }
 
 - (void)resetAllAppPermissions {
     SBSApplicationTerminationAssertionRef assertion = SBSApplicationTerminationAssertionCreateWithError(NULL, self.bundleIdentifier, 1, NULL);
+    
     [self _resetAllAppPermissions];
+    
     if (assertion) {
         SBSApplicationTerminationAssertionInvalidate(assertion);
     }
 }
 
 - (void)_resetAllAppPermissions {
-    if (@available(iOS 16.0, *)) {
-        // iOS 16 及以上：直接操作 TCC.db 删除该 App 的所有权限记录
-        NSString *dbPath = @"/private/var/mobile/Library/TCC/TCC.db";
-        sqlite3 *db;
-        
-        if (sqlite3_open([dbPath UTF8String], &db) == SQLITE_OK) {
-            NSString *query = [NSString stringWithFormat:@"DELETE FROM access WHERE client = '%@'", self.bundleIdentifier];
-            char *errMsg;
-            if (sqlite3_exec(db, [query UTF8String], NULL, NULL, &errMsg) != SQLITE_OK) {
-                NSLog(@"[AppData] Failed to delete from TCC.db: %s", errMsg);
-                sqlite3_free(errMsg);
-            }
-            sqlite3_close(db);
-        }
-        
-        // 重启 tccd 让缓存失效，达到万无一失
-        [self restartTCCD];
-        
-        // 位置权限由 locationd 独立管理，继续使用 CLLocationManager 重置
-        [CLLocationManager setAuthorizationStatusByType:kCLAuthorizationStatusNotDetermined
-                                    forBundleIdentifier:self.bundleIdentifier];
-    } else {
-        // iOS 15 及以下：原有的私有 API 逻辑
-        CFBundleRef bundle = CFBundleCreate(kCFAllocatorDefault, (CFURLRef)self.appProxy.bundleURL);
-        if (bundle) {
-            TCCAccessResetForBundle(kTCCServiceAll, bundle);
-            CFRelease(bundle);
-        }
-        
-        // 重置位置权限
-        [CLLocationManager setAuthorizationStatusByType:kCLAuthorizationStatusNotDetermined
-                                    forBundleIdentifier:self.bundleIdentifier];
+    CFBundleRef bundle = CFBundleCreate(kCFAllocatorDefault, (CFURLRef)self.appProxy.bundleURL);
+    if (bundle) {
+        TCCAccessResetForBundle(kTCCServiceAll, bundle);
+        CFRelease(bundle);
     }
+    
+    // Reset location permission
+    [CLLocationManager setAuthorizationStatusByType:kCLAuthorizationStatusNotDetermined forBundleIdentifier:self.bundleIdentifier];
 }
 
 #pragma mark - Reset App
@@ -379,7 +317,7 @@
 
 - (NSArray *)appGroupDirectoryURLs {
     NSMutableArray *appGroupDirectoryURLs = [NSMutableArray new];
-    for (ADAppDataGroup *group in self.appGroups) {
+    for (ADAppDataGroup *group in self.appGroups){
         [appGroupDirectoryURLs addObject:group.url];
     }
     return appGroupDirectoryURLs;
@@ -403,7 +341,7 @@
 - (void)getAppUsageDirectorySizeWithCompletion:(void(^)(NSString *formattedSize))completion {
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
         unsigned long long int totalSize = 0;
-        NSArray<NSURL *> *appUsageDirectoriesURLs = [self appUsageDirectoriesURLs];
+        NSArray <NSURL *> *appUsageDirectoriesURLs = [self appUsageDirectoriesURLs];
         for (NSURL *url in appUsageDirectoriesURLs) {
             if (url && [[NSFileManager defaultManager] fileExistsAtPath:url.path]) {
                 unsigned long long int folderSize = 0;
@@ -411,36 +349,32 @@
                 totalSize += folderSize;
             }
         }
-        NSString *formattedSize = [NSByteCountFormatter stringFromByteCount:totalSize
-                                                                 countStyle:NSByteCountFormatterCountStyleFile];
+        NSString *formattedSize = [NSByteCountFormatter stringFromByteCount:totalSize countStyle:NSByteCountFormatterCountStyleFile];
         dispatch_async(dispatch_get_main_queue(), ^{
             completion(formattedSize);
         });
     });
 }
 
-- (void)resetDiskContentWithCompletion:(void(^)(void))completion {
+- (void)resetDiskContentWithCompletion:(void(^)())completion {
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
         SBSApplicationTerminationAssertionRef assertion = SBSApplicationTerminationAssertionCreateWithError(NULL, self.bundleIdentifier, 1, NULL);
         
-        NSArray<NSURL *> *appUsage = [self appUsageDirectoriesURLs];
+        NSArray <NSURL *> *appUsage = [self appUsageDirectoriesURLs];
         for (NSURL *url in appUsage) {
             [self.class deleteContentsOfDirectoryAtURL:url];
         }
         
         // Recreate Preferences folder
         if ([self appLibraryDirectoryURL]) {
-            [[NSFileManager defaultManager] createDirectoryAtURL:[[self appLibraryDirectoryURL] URLByAppendingPathComponent:@"Preferences" isDirectory:YES]
-                                     withIntermediateDirectories:YES
-                                                      attributes:nil
-                                                           error:NULL];
+            [[NSFileManager defaultManager] createDirectoryAtURL:[[self appLibraryDirectoryURL] URLByAppendingPathComponent:@"Preferences" isDirectory:YES] withIntermediateDirectories:YES attributes:nil error:NULL];
         }
         
         // Reset all permissions (这里保留我们修改过的 isApplication 逻辑)
         if (self.isApplication) {
             [self _resetAllAppPermissions];
         }
-        
+
         if (assertion) {
             SBSApplicationTerminationAssertionInvalidate(assertion);
         }
@@ -452,7 +386,6 @@
 }
 
 #pragma mark - Uninstall App
-
 // 这里的卸载逻辑保留了我们改写的彻底卸载
 - (void)uninstallAppWithCompletion:(void(^)(BOOL success))completion {
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
